e07d58a872b44b49ef249255ca154cba2d3420e7
mario <46033731+wifimarie@users.noreply.github.com>
2019-03-08 12:19:17 -0400

    meta añadido
    cambie el color y el titulo del blog

--- public_html_proyecto/index.php
+++ public_html_proyecto/index.php
@@ -1,13 +1,13 @@
 <?php
-$saludo = "Bienvenidos a mi sitio web daydreammarie";
+$saludo = "Bienvenidos a mi sitio web kulturizate";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#0FB2EB";
     <title><?php echo $saludo; ?></title>
 </head>
 <body>
