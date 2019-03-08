5cf3c2ead8ae0d4959347facd9f844add1a4a272
mario <46033731+wifimarie@users.noreply.github.com>
2019-03-08 08:37:54 -0400

    pequeños cambios
    se ha creado dos index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = "Cristian"; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bienvenido <?php echo $nombre; ?></title>
+</head>
+<body>
+    
+    Esto es un documento php.
+
+    <?php
+    echo ("Este codigo es de php");
+ ?>
+
+
+<p> Hola <?php $nombre; ?></p>
+
+</body>
+</html>
---
+++ php_marie/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marie
+ * Date: 07/03/2019
+ * Time: 09:12 PM
+ */
+
+echo " Hola people";
+
