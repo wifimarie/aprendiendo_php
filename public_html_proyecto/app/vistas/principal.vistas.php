5fca7bf0d8b97ffd2113fddf6e3282809c810bd2
mario <46033731+wifimarie@users.noreply.github.com>
2019-03-18 11:04:01 -0400

    2

--- public_html_proyecto/app/vistas/principal.vistas.php
+++ public_html_proyecto/app/vistas/principal.vistas.php
@@ -1,7 +1,38 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: estudiante
- * Date: 15/3/19
- * Time: 10:24 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="recursos/css/bootstrap.css">
+    <link rel="shortcut icon" href="recursos/img/icon.ico" type="image/ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+        crossorigin="anonymous">
+    <meta name="theme-color" content="#222221" ;> <!-- color del meta en google chrome-->
+    <title>Bandeja de entrada | kulturizate</title>
+</head>
+<body>
+    <!-- As a link -->
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Kulturizate</a>
+    </nav>
+    
+    <div class="jumbotron">
+        <h1 class="display-5">Bienvenido
+            
+        </h1>
+        <p class="lead">Esta es la pagina principal</p>
+        <hr class="my-4">
+        <a class="btn btn-danger btn-lg" href="#" role="button">Cerrar sesion</a>
+    </div>
+
+    <!-- scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
